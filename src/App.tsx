86efa76c2f8703a916c3d14a2a42d76742ec5709
이종원 <jongwon@ijong-won-ui-MacBookPro.local>
2025-03-04 00:29:59 +0900

    feat: react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import styled from "styled-components";
+import Login from "components/Login";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+`;
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Container>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </Container>
+  );
 }
 
 export default App;
